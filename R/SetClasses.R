500d31190f06e032c559e5d096dcef8b137e7811
Greg Finak <greg.finak@ircm.qc.ca>
2013-02-08 00:58:25 +0000

    Update to flowClust 2.99, ready to move to 3. Merged into current devel svn
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowClust@73270 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SetClasses.R
+++ R/SetClasses.R
@@ -6,7 +6,7 @@ setClass("flowClust",
                         uncertainty="vector", ruleOutliers="vector",
                         flagOutliers="vector", rm.min="numeric",
                         rm.max="numeric", logLike="numeric", BIC="numeric",
-                        ICL="numeric"),
+                        ICL="numeric", usePrior="character", prior="list"),
          prototype(expName=character(0), varNames=character(0), K=numeric(0),
                    w=rep(numeric(0),0), mu=matrix(numeric(0), nrow=0, ncol=0),
                    sigma=array(numeric(0), c(0,0,0)), lambda=numeric(0),
@@ -16,8 +16,28 @@ setClass("flowClust",
                    ruleOutliers=rep(numeric(0),0),
                    flagOutliers=rep(logical(0), 0), rm.min=numeric(0),
                    rm.max=numeric(0), logLike=numeric(0), BIC=numeric(0),
-                   ICL=numeric(0)))
+                   ICL=numeric(0), usePrior="no", prior=list(NA)))
 
+
+setClass("flowClustPriorTree", contains="graphNEL",representation(name="character"))
+setMethod("initialize",signature="flowClustPriorTree",definition=function(.Object,...){
+	.Object<-callNextMethod(.Object,edgemode="directed")
+	nodeDataDefaults(.Object,"prior")<-NA;
+	nodeDataDefaults(.Object,"gate")<-NA;
+	return(.Object);
+})
+
+setClass("flowClustTree",contains="graphNEL",representation(name="character",priortree="flowClustPriorTree"),
+	prototype(name=character(0),priortree=new("flowClustPriorTree")));
+setMethod("initialize",signature="flowClustTree",definition=function(.Object,priortree,...){
+	.Object<-callNextMethod(.Object,edgemode="directed")
+	.Object@priortree=priortree;
+	nodeDataDefaults(.Object,"model")<-NA;
+	nodeDataDefaults(.Object,"population")<-NA;
+	nodeDataDefaults(.Object,"name")<-NA;
+	nodeDataDefaults(.Object,"gate")<-NA;
+	return(.Object)
+})
 
 setClass("flowClustList",
          representation("list", index="numeric", criterion="character"),
@@ -44,12 +64,13 @@ setClass("tmixFilter",
                         max.count="numeric", min="vector", max="vector",
                         level="numeric", u.cutoff="numeric", z.cutoff="numeric",
                         randomStart="numeric", B.init="numeric", tol.init="numeric",
-                        seed="numeric", criterion="character", control="list"),
+                        seed="numeric", criterion="character", control="list",
+                        usePrior="character", prior="list"),
          prototype(expName="Flow Experiment", K=numeric(0), B=500, tol=1e-5,
                    nu=4, lambda=1, nu.est=0, trans=1, min.count=10, max.count=10,
                    min=NA, max=NA, level=0.9, u.cutoff=NA_real_, z.cutoff=0,
                    randomStart=10, B.init=500, tol.init=1e-2, seed=1, criterion="BIC", 
-                   control=vector("list",0)),
+                   control=vector("list",0), usePrior="no", prior=list(NA)),
          contains="parameterFilter")
 
 
@@ -64,5 +85,5 @@ tmixFilter <- function(filterId="tmixFilter", parameters="", ...)
 setClass("tmixFilterResult", contains=c("flowClust", "multipleFilterResult"))
 
 
-setClass("tmixFilterResultList",
-         contains=c("flowClustList", "multipleFilterResult"))
+setClassUnion("tmixFilterResultList",
+         members=c("flowClustList", "multipleFilterResult"))
--- src/BoxCoxPriors.c
+++ src/BoxCoxPriors.c
@@ -1,6 +1,13 @@
+/*
+ * BoxCoxPriors.c
+ *
+ *  Created on: Oct 21, 2010
+ *      Author: finak
+ */
+#include <string.h>
 #include "flowClust.h"
 
-double BoxCoxGradient(double x, void *params) {
+double BoxCoxGradientPriors(double x, void *params) {
 	struct BoxCox_params *p = (struct BoxCox_params *) params;
 	gsl_matrix *Y = p->Y;
 	gsl_vector *W = p->W; // will be updated
@@ -16,7 +23,13 @@ double BoxCoxGradient(double x, void *params) {
 	gsl_vector *SumZlogY = p->SumZlogY;
 	gsl_matrix *DiagOne = p->DiagOne;
 	gsl_matrix *Mu0 = p->Mu0;
+	gsl_matrix *Omega0 = p->Omega0;
 	gsl_matrix *Lambda0 = p->Lambda0;
+	int model = *p->model;
+	int *oorder = p->oorder;
+	double *w0 = p->w0;
+	int iter = p->iter;
+	int *itersolve = p->itersolve;
 	double *nu0 = p->nu0, kappa0 = p->kappa0;
 
 	int K1 = p->K;
@@ -25,7 +38,7 @@ double BoxCoxGradient(double x, void *params) {
 	int j = 0, py = Mu->size2;
 	int k = 0, K = Mu->size1;
 	int K0 = 0;
-
+	
 	double Yoriginal = 0, Tmp = 0, Tmp1 = 0, Tmp2 = 0, Tmp3 = 0;
 	double logJacobian = 0; // derivative of log(Jacobian) wrt lambda
 	double logLike = 0; // derivative of loglikelihood wrt lambda
@@ -33,11 +46,24 @@ double BoxCoxGradient(double x, void *params) {
 	gsl_matrix *ZUY1 = gsl_matrix_alloc(ly, K * py);
 	gsl_matrix *PrecisionYTrans = gsl_matrix_alloc(ly, py);
 	gsl_vector *Y1Sum = gsl_vector_alloc(py);
+	gsl_matrix *tmpOmega = gsl_matrix_calloc(Omega0->size1,Omega0->size2);
+	gsl_matrix *tmpLambda0 = gsl_matrix_calloc(K,py*py);
+	gsl_matrix *tmpMu0 = gsl_matrix_calloc(K,py);
+	gsl_matrix *MuGMatrix = gsl_matrix_calloc(ly, py);
+	gsl_matrix *ZY = gsl_matrix_calloc(K, py); // ZU*YTrans
 	gsl_vector *SMu = gsl_vector_alloc(py);
 	gsl_matrix_view matrixZUY, matrixZUY1, matrixPrecision, matrixLambda0;
 	gsl_vector_view rowZUY, rowZUY1, rowPrecisionYTrans, rowMu, rowMu0,
 	rowPrecision, rowZ, rowLambda0;
-
+	double w0sum=0;
+	
+	for(int ii=0;ii<K1;ii++){
+		w0sum=w0sum+w0[ii];
+	}
+	if(K1!=-1){
+		//Rprintf("K1=%d",K1);
+		error("Estimation of lambda with priors is not implemented for cluster specific degrees of freedom or cluster specific lambda.\n");
+	}
 	for (i = 0; i < ly; i++) {
 		for (j = 0; j < py; j++) {
 			/* Compute YTrans */
@@ -48,17 +74,11 @@ double BoxCoxGradient(double x, void *params) {
 			Tmp2 = gsl_matrix_get(logY, i, j) * x - 1.0;
 			Tmp3 = (Tmp1 * Tmp2 + 1.0) / gsl_pow_2(x);
 			gsl_matrix_set(Y1, i, j, Tmp3);
-
-			if (K1 == -1)
-				logJacobian += gsl_matrix_get(logY, i, j);
+			logJacobian += gsl_matrix_get(logY, i, j);
 		}
 	}
-	if (K1 > -1)
-		logJacobian = gsl_vector_get(SumZlogY, K1);
 
 	/* Update ZUY-matrix and compute ZUY1 */
-	if (K1 > -1)
-		K0 = K1;
 	for (k = K0; k < K; k++) {
 		matrixZUY = gsl_matrix_submatrix(ZUY, 0, k * py, ly, py);
 		gsl_matrix_memcpy(&matrixZUY.matrix, YTrans);
@@ -73,39 +93,74 @@ double BoxCoxGradient(double x, void *params) {
 			rowZUY1 = gsl_matrix_row(&matrixZUY1.matrix, i);
 			gsl_blas_dscal(gsl_matrix_get(Z, i, k), &rowZUY1.vector);
 		}
-
-		if (K1 > -1)
-			break;
 	}
 
-	/* Update Mu using BLAS */
-	gsl_matrix_memcpy(Mu, Mu0);
-	if (K1 == -1) {
-		gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, Z, YTrans, kappa0, Mu);
-	}
+	/* Update Mu  */
+	gsl_matrix_memcpy(p->PrecisionUpdate,p->Precision);
+
+	/* Use copies of the priors */
+	gsl_matrix_memcpy(tmpOmega, Omega0);
+	gsl_matrix_memcpy(tmpMu0,Mu0);
+	gsl_matrix_memcpy(tmpLambda0,Lambda0);
+	double* tmpnu0=calloc(K,sizeof(double));
+	memcpy(tmpnu0,nu0,K*sizeof(double));
+	int* tmpoorder=calloc(K,sizeof(int));
+	double* tmpw0 = calloc(K,sizeof(double));
+	memcpy(tmpoorder,oorder,K*sizeof(int));
+	memcpy(tmpw0,w0,K*sizeof(double));
+	
+	/*Assume Omega0 is already inverted*/
+	/*  Mu = Omega0^{-1} * Mu0*/
+	//gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, tmpMu0, tmpOmega,
+	//		0.0, p->MuUpdate);
+	/* ZY = Z*Y */// dim(k*p)
+	gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, Z, YTrans, 0.0, ZY);
 	for (k = K0; k < K; k++) {
-		rowMu = gsl_matrix_row(Mu, k);
-		rowMu0 = gsl_matrix_row(Mu0, k);
+		gsl_vector_view rowOmega0 = gsl_matrix_row(tmpOmega,k);
+		gsl_matrix_view matrixOmega0 = gsl_matrix_view_vector(&rowOmega0.vector,py,py);
+		rowMu = gsl_matrix_row(p->MuUpdate, k);
+		rowMu0=gsl_matrix_row(tmpMu0,k);
 
-		if (K1 > -1) {
-			rowZ = gsl_matrix_column(Z, k);
-			gsl_blas_dgemv(CblasTrans, 1.0, YTrans, &rowZ.vector, kappa0,
-					&rowMu.vector);
-		}
-		gsl_blas_dscal(1. / (gsl_vector_get(SumZU, k) + kappa0), &rowMu.vector);
+		gsl_blas_dgemv(CblasNoTrans, 1.0,
+				&matrixOmega0.matrix, &rowMu0.vector, 0.0, &rowMu.vector);
 
-		/* Update Precision (cluster specific) */
-		rowPrecision = gsl_matrix_row(Precision, k);
-		matrixPrecision = gsl_matrix_view_vector(&rowPrecision.vector, py, py);
-		rowLambda0 = gsl_matrix_row(Lambda0, k);
+
+		rowPrecision = gsl_matrix_row(p->PrecisionUpdate, k);
+		matrixPrecision
+		= gsl_matrix_view_vector(&rowPrecision.vector, py, py);
+
+		gsl_vector_view rowZY = gsl_matrix_row(ZY, k);
+		ECMUpdateMUg(&matrixPrecision.matrix, &rowMu.vector, &rowZY.vector,
+				&matrixOmega0.matrix, gsl_vector_get(SumZU, k), DiagOne);
+	}
+	reorderPriors(p->MuUpdate,tmpMu0,tmpLambda0,tmpOmega,tmpnu0,tmpw0,tmpoorder);
+	/* Update Sigma */
+	for (k = K0; k < K; k++) {
+		rowPrecision = gsl_matrix_row(p->PrecisionUpdate, k);
+		matrixPrecision
+		= gsl_matrix_view_vector(&rowPrecision.vector, py, py);
+		rowLambda0 = gsl_matrix_row(tmpLambda0, k);
 		matrixLambda0 = gsl_matrix_view_vector(&rowLambda0.vector, py, py);
 		matrixZUY = gsl_matrix_submatrix(ZUY, 0, k * py, ly, py);
-		up_date_precision(&matrixZUY.matrix, &rowMu.vector,
-				&matrixPrecision.matrix, gsl_vector_get(SumZ, k),
-				gsl_vector_get(SumZU, k), DiagOne, &rowMu0.vector,
-				&matrixLambda0.matrix, kappa0, nu0[k]);
+		rowMu = gsl_matrix_row(p->MuUpdate, k);
+		if (gsl_matrix_isnull(MuGMatrix)) {
+			for (i = 0; i < ly; i++) {
+				gsl_matrix_set_row(MuGMatrix, i, &rowMu.vector);
+			}
+		}
+		gsl_matrix_memcpy(&matrixPrecision.matrix, &matrixLambda0.matrix);
+		gsl_vector_view zu = gsl_matrix_column(U, k);
+		ECMUpdateSigmaG2(&zu.vector, YTrans, MuGMatrix,
+				&matrixPrecision.matrix, gsl_vector_get(SumZ, k), nu0[k]);
+		gsl_matrix_set_zero(MuGMatrix);
+	}
+	for (k = K0; k < K; k++) {
 		/* Update Mixing Proportions */
 		gsl_vector_set(W, k, gsl_vector_get(SumZ, k) / ly);
+		gsl_vector_set(W, k, (gsl_vector_get(SumZ, k)+w0[k])
+				/ (w0sum+(ly)));
+
+		rowMu = gsl_matrix_row(p->MuUpdate, k);
 
 		/* Compute PrecisionYTrans = YTrans times sqrt(Precision) */
 		gsl_matrix_memcpy(PrecisionYTrans, YTrans);
@@ -143,26 +198,23 @@ double BoxCoxGradient(double x, void *params) {
 		/* Compute SMu times Y1Sum */
 		gsl_blas_ddot(SMu, Y1Sum, &Tmp);
 		logLike += Tmp;
-
-		if (K1 > -1)
-			break;
 	}
 
 	logLike += logJacobian;
+	//Rprintf("logLikeDeriv=%f\n",logLike);
 
 	gsl_vector_free(Y1Sum);
 	gsl_vector_free(SMu);
+	gsl_matrix_free(ZY);
 	gsl_matrix_free(Y1);
+	gsl_matrix_free(tmpOmega);
+	gsl_matrix_free(tmpLambda0);
+	gsl_matrix_free(tmpMu0);
 	gsl_matrix_free(PrecisionYTrans);
 	gsl_matrix_free(ZUY1);
+	gsl_matrix_free(MuGMatrix);
+	free(tmpnu0);
+	free(tmpoorder);
+	free(tmpw0);
 	return (logLike);
 }
-
-
-int sgn(double x) {
-	if (x >= 0)
-		return 1;
-	else
-		return -1;
-}
-
