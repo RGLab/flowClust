5356b3c8a75cb8dbc7e0967158f1da5bbec309f8
Mike Jiang <wjiang2@fhcrc.org>
2016-10-08 00:48:48 +0000

    add unit test for 1d flowClust
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowClust@122083 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testthat.R
@@ -0,0 +1,9 @@
+library(testthat)
+library(flowClust)
+library(parallel)
+test_check("flowClust")
+
+#devtools::test("~/rglab/workspace/flowClust")
+
+#taking quite some time , thus only for internal testing
+#test_file("~/rglab/workspace/flowClust/tests/testthat/flowClust-gated-data.R")
