03eb55ac4e6c300109128ad540429248b5659506
JACOB WAGNER <jpwagner@fredhutch.org>
2018-10-24 15:43:22 -0700

    Some roxygen2 comment/doc fixes

--- R/miscellaneous.R
+++ R/miscellaneous.R
@@ -135,7 +135,6 @@ posterior <- function(object, assign=FALSE)
 #' @name BIC
 #' @title Extract the BIC for a flowClust fit.
 #' @importFrom stats BIC
-#' @method BIC.flowClustList
 #' @rdname BIC
 #' @return \code{vector} of BIC or ICL values
 #' @param object \code{flowClustList} or \code{flowClust} fitted object
@@ -145,11 +144,8 @@ BIC.flowClustList = function(object,...){
 	criterion(object,type="BIC")
 }
 
-#' @method BIC.flowClust
 #' @rdname BIC
 #' @return \code{vector} of BIC or ICL values
-#' @param object \code{flowClustList} or \code{flowClust} fitted object
-#' @param ... other arguments. Currently not used.
 #' @export
 BIC.flowClust = function(object,...){
 	criterion(object,type="BIC")
