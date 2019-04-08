16979ddf341a542e8a03a51c88e6285f65189a78
gfinak <greg.finak@gmail.com>
2019-04-08 13:10:18 -0700

    add tests

--- tests/testthat/test_2d.R
+++ tests/testthat/test_2d.R
@@ -9,9 +9,17 @@ test_that("flowClust 2d: prior", {
   
   prior <- prior_flowClust(fs ,c("FSC-A", "SSC-A"), K = 2)
   options("mc.cores" = 1)  
-  g <- flowClust.2d(fs[[1]], "FSC-A", "SSC-A", K=2,target=c(1e5,5e4), prior = prior, usePrior = "yes")
-
+  g <-
+  	flowClust.2d(
+  		fs[[1]],
+  		"FSC-A",
+  		"SSC-A",
+  		K = 2,
+  		target = c(1e5, 5e4),
+  		prior = prior,
+  		usePrior = "yes"
+  	)
+  
   # expect_equal(g@boundaries, matrix(c(5.27, 6.84), nrow = 2))
 })
-
 
