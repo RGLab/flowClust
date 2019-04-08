224b750aaa42fa450516327d08c7519ad6d1025d
mikejiang <wjiang2@fhcrc.org>
2019-04-08 16:52:44 -0700

    complete test 2d

--- tests/testthat/test_2d.R
+++ tests/testthat/test_2d.R
@@ -6,7 +6,7 @@ Sys.setenv("_R_CHECK_LIMIT_CORES_" = "warn")
 
 
 test_that("flowClust 2d: prior", {
-  
+  set.seed(1)
   prior <- prior_flowClust(fs ,c("FSC-A", "SSC-A"), K = 2)
   options("mc.cores" = 1)  
   g <-
@@ -20,6 +20,11 @@ test_that("flowClust 2d: prior", {
   		usePrior = "yes"
   	)
   
-  # expect_equal(g@boundaries, matrix(c(5.27, 6.84), nrow = 2))
+  expect_equal(g@cov, structure(c(1173858741.42853, 523766402.261612, 523766402.261612, 625519945.097251)
+                                , .Dim = c(2L, 2L)
+                                , .Dimnames = list(c("FSC-A", "SSC-A"), c("FSC-A", "SSC-A")))
+              )
+  expect_equal(g@distance, 2.94, tolerance =1e-3)
+  expect_equal(g@mean, c("FSC-A" = 87638.359375, "SSC-A" = 62425.46875))
 })
 
